b4416a9985776eaedabb67b4ab84b871d979471c
Igor Derevnin <shroom2207@yandex.ru>
2024-11-07 00:48:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+import argparse
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from collections import defaultdict
 from datetime import date
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import pandas
-
-FOUNDATION_YEAR = 1920
 
 
 def count_passed_years(first_year: int):
@@ -13,8 +12,11 @@ def count_passed_years(first_year: int):
 
 
 def correct_years_text_case(years_passed):
-    years_stringed = str(years_passed)
-    last_numbers = years_stringed[-2:]
+    if years_passed < 10:
+        years_stringed = '0' + str(years_passed)
+    else:
+        years_stringed = str(years_passed)
+    last_numbers = years_stringed[-2:]    
     if last_numbers[1] == '1' and last_numbers[0] != '1':
         return 'год'
     if last_numbers[1] in ['2', '3', '4'] and last_numbers[0] != '1':
@@ -29,10 +31,29 @@ def group_wines(wines_list):
         for wine in wines_list:
             if wine['wine_category'] == category:
                 groupped_wines[category].append(wine)
-    return (groupped_wines)
+    return groupped_wines
 
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description='''Установите входные параметры:
+        -p  Путь к xlsx файлу, default='wine.xlsx'
+        -s  Название листа с таблицей, default='Sheet1'
+        -y  Год основания винодельни, default=1920
+        ''')
+    parser.add_argument('-p','--table_path', help='Путь к xlsx файлу', type=str, default='wine.xlsx')
+    parser.add_argument('-s','--sheet_name', help='Название листа с таблицей', type=str, default='Sheet1')
+    parser.add_argument('-y','--foundation_year', help='Год основания винодельни', type=int, default=1920)
+    return parser
+    
 
 def main():
+    
+    parser = create_parser()
+    args = parser.parse_args()
+    table_path = args.table_path
+    sheet_name = args.sheet_name
+    foundation_year = args.foundation_year
+   
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -40,12 +61,12 @@ def main():
 
     template = env.get_template('template.html')
 
-    years_passed = count_passed_years(FOUNDATION_YEAR)
+    years_passed = count_passed_years(foundation_year)
 
-    wines_table = pandas.read_excel('your_excel_file.xlsx', sheet_name='Sheet1', na_values=[
+    wines_table = pandas.read_excel(table_path, sheet_name=sheet_name, na_values=[
                                     'N/A', 'NA'], keep_default_na=False)
-    wines_list = wines_table.to_dict(orient='records')
-    groupped_wines = group_wines(wines_list)
+    wines_data = wines_table.to_dict(orient='records')
+    groupped_wines = group_wines(wines_data)
 
     rendered_page = template.render(
         groupped_wines=groupped_wines,
